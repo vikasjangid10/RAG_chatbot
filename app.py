39bfea935f27e422765affa6cd002458afe9f7ee
vikasjangid10 <v7776139@gmail.com>
2025-07-30 17:48:11 +0530

    chroma removed

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 from langchain.chains import create_history_aware_retriever, create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
-from langchain_chroma import Chroma
 from langchain_community.chat_message_histories import ChatMessageHistory
 from langchain_core.chat_history import BaseChatMessageHistory
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
